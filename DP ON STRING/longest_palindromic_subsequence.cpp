b2c263f4ec620e9415c660310d7346b75c7ed3f3
Unknownmaster0 <sagar192002singh@gmail.com>
2024-03-24 12:21:07 +0530

    corrected the indentation

--- DP ON STRING/longest_palindromic_subsequence.cpp
+++ DP ON STRING/longest_palindromic_subsequence.cpp
@@ -10,19 +10,19 @@ using namespace std;
                                             Just traverse the same string.
 */
 int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n));
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i][i] = 1;
-            for (int j = i+1; j < n; ++j) {
-                if (s[i] == s[j]) {
-                    dp[i][j] = dp[i+1][j-1] + 2;    // if we found the both character match, then we can add both character to the palindrome that's why I added 2 here.
-                } else {
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]); // else not matches, then simply take the max of the (left and down).
-                }
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int i = n - 1; i >= 0; --i) {
+        dp[i][i] = 1;
+        for (int j = i+1; j < n; ++j) {
+            if (s[i] == s[j]) {
+                dp[i][j] = dp[i+1][j-1] + 2;    // if we found the both character match, then we can add both character to the palindrome that's why I added 2 here.
+            } else {
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1]); // else not matches, then simply take the max of the (left and down).
             }
         }
-        return dp[0][n-1];
+    }
+    return dp[0][n-1];
 }
 
 
